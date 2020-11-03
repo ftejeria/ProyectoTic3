506c464396d3710901e71951339751579398b82e
ftejeria <tejeria.felipe@gmail.com>
2020-11-02 23:32:47 -0300

    filtros

--- public/js/examenes.js
+++ public/js/examenes.js
@@ -3,33 +3,32 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.json())
     .then((data) => loadHTMLTable(data["data"]));
 });
-const addBtn = document.querySelector('#addBtn');
+const addBtn = document.querySelector("#addBtn");
 
-document.querySelector('table tbody').addEventListener('click', function(event) {
-  if (event.target.className === "delete-row-btn") {
+document
+  .querySelector("table tbody")
+  .addEventListener("click", function (event) {
+    if (event.target.className === "delete-row-btn") {
       deleteRowById(event.target.dataset.id);
-  }
-  if (event.target.className === "edit-row-btn") {
+    }
+    if (event.target.className === "edit-row-btn") {
       handleEditRow(event.target.dataset.id);
-  }
-});
+    }
+  });
 
 function deleteRowById(id) {
-  fetch('http://localhost:5000/delete/' + id, {
-      method: 'DELETE'
+  fetch("http://localhost:5000/delete/" + id, {
+    method: "DELETE",
   })
-  .then(response => response.json())
-  .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       if (data.success) {
-          location.reload();
+        location.reload();
       }
-  });
+    });
 }
 
-const updateBtn = document.querySelector('#update-row-btn');
-
 addBtn.onclick = function () {
- 
   const nameInput = document.querySelector("#name-input");
 
   const periodoInput = document.querySelector("#periodo-input");
@@ -41,7 +40,7 @@ addBtn.onclick = function () {
   const fecha = fechaInput.value;
   const periodo = periodoInput.value;
   const semestre = semestreInput.value;
-  const salon= salonInput.value;
+  const salon = salonInput.value;
   const horario = horarioInput.value;
   nameInput.value = "";
   fechaInput.value = "";
@@ -49,7 +48,6 @@ addBtn.onclick = function () {
   semestreInput.value = "";
   salonInput.value = "";
   horarioInput.value = "";
-
 
   fetch("http://localhost:5000/insert", {
     headers: {
@@ -61,8 +59,8 @@ addBtn.onclick = function () {
       fecha: fecha,
       periodo: periodo,
       semestre: semestre,
-      salon : salon,
-      horario : horario
+      salon: salon,
+      horario: horario,
     }),
   })
     .then((response) => response.json())
@@ -83,8 +81,6 @@ function insertRowIntoTable(data) {
       tableHtml += `<td>${data[key]}</td>`;
     }
   }
-
-
 
   tableHtml += "</tr>";
 
@@ -107,8 +103,21 @@ function loadHTMLTable(data) {
 
   let tableHtml = "";
 
-  data.forEach(function ({ id_exam, nombre, fecha,periodo,semestre, salon,horario}) {
-    fecha =new Date(fecha).getDate() + "/" +new Date(fecha).getMonth() + "/"+ new Date(fecha).getFullYear()
+  data.forEach(function ({
+    id_exam,
+    nombre,
+    fecha,
+    periodo,
+    semestre,
+    salon,
+    horario,
+  }) {
+    fecha =
+      new Date(fecha).getDate() +
+      "/" +
+      new Date(fecha).getMonth() +
+      "/" +
+      new Date(fecha).getFullYear();
     tableHtml += "<tr>";
     tableHtml += `<td>${id_exam}</td>`;
     tableHtml += `<td>${nombre}</td>`;
@@ -123,35 +132,119 @@ function loadHTMLTable(data) {
   });
 
   table.innerHTML = tableHtml;
-  
 }
 
-function handleEditRow(id) {
-  const updateSection = document.querySelector('#update-row');
-  updateSection.hidden = false;
-  document.querySelector('#update-name-input').dataset.id = id;
-}
+function buscarNombre() {
+  var input, filter, table, tr, td, i, txtValue,txtValue2,txtValue3;
+  input = document.getElementById("myInputNombre");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
 
-updateBtn.onclick = function() {
-  const updateNameInput = document.querySelector('#update-name-input');
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().startsWith(filter)) {
+        tr[i].style.display = "";
 
-
-  console.log(updateNameInput);
-
-  fetch('http://localhost:5000/update', {
-      method: 'PATCH',
-      headers: {
-          'Content-type' : 'application/json'
-      },
-      body: JSON.stringify({
-          id: updateNameInput.dataset.id,
-          name: updateNameInput.value
-      })
-  })
-  .then(response => response.json())
-  .then(data => {
-      if (data.success) {
-          location.reload();
+      } else {
+        tr[i].style.display = "none";
       }
-  })
+    }
+  }
 }
+
+function buscarSemestre(){
+  var input, filter, table, tr, td, i, txtValue,txtValue2,txtValue3;
+  input = document.getElementById("myInputNombre");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  input2 = document.getElementById("myInputSemestre");
+  filter2 = input2.value.toUpperCase();
+  if (input2.value != "") {
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[4];
+      if (td) {
+        txtValue2 = td.textContent || td.innerText;
+        if (txtValue2.toUpperCase().startsWith(filter2) ) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
+
+}
+
+function buscarPeriodo(){
+  var input, filter, table, tr, td, i, txtValue,txtValue2,txtValue3;
+  input = document.getElementById("myInputNombre");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  input3 = document.getElementById("myInputPeriodo");
+  filter3 = input3.value.toUpperCase();
+
+  if (input3.value != "") {
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[3];
+        txtValue3 = td.textContent || td.innerText;
+        if (txtValue3.toUpperCase()==filter3 ) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      
+    }
+  }
+
+}
+
+function buscar (){
+
+  var input, filter, table, tr, td, i, txtValue,txtValue2,txtValue3;
+  input = document.getElementById("myInputNombre");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  input2 = document.getElementById("myInputSemestre");
+  filter2 = input2.value.toUpperCase();
+  input3 = document.getElementById("myInputPeriodo");
+  filter3 = input3.value.toUpperCase();
+
+
+
+  if (input.value != "" && input2.value != "" && input3.value != "" ){
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      td2 = tr[i].getElementsByTagName("td")[3];
+      td3 = tr[i].getElementsByTagName("td")[4];
+      if(td && td2 && td3){
+        txtValue = td.textContent || td.innerText;
+        txtValue2 = td2.textContent || td2.innerText;
+        txtValue3 = td3.textContent || td3.innerText;
+
+        if (txtValue3.toUpperCase()==filter3 && txtValue2.toUpperCase()==filter2 && txtValue.toUpperCase().startsWith(filter) ) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+      
+    }
+
+  }
+  else if (input.value == "" && input2.value == "" && input3.value == "" ) {
+    for (i = 0; i < tr.length; i++) {
+        tr[i].style.display = "";
+        
+    }
+  }
+
+}
+
+
