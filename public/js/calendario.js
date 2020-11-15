30abd21b2f05a5e0f15e67109df55fedfc844964
pipe <felipetejeriaolaso@gmail.com>
2020-11-15 10:51:53 -0300

    m

--- public/js/calendario.js
+++ public/js/calendario.js
@@ -81,11 +81,11 @@ function showCalendar(month, year, data) {
     months[month] +
     " " +
     year +
-    '<button class="btn btn-outline-primary botones" id="previous" onclick="previous()" style="margin-left:37%; background-color: #004e91; color:white;">' +
-    "Mes anterior" +
-    "</button>" +
+    '<button class="btn btn-outline-primary botones" id="previous" onclick="previous()" style="margin-left:37%; background-color: #004e91; color:white;">' + '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-left-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="padding-top: 4px;"><path d="M3.86 8.753l5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/> </svg>'+
+    " <span style:margin-top:0px> Mes anterior </span>" + 
+     "</button>" +
     '<button class="btn btn-outline-primary  "id="next"onclick="next()" style="background-color: #004e91; color:white;"> ' +
-    "Proximo mes" +
+    "<span style:margin-top:0px>Proximo mes </span>"  + ' <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-right-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="padding-top: 4px;"><path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg>'+
     " </button>";
   selectYear.value = year;
   selectMonth.value = month;
